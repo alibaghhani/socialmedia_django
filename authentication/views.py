b15dc5303336fcb1eb5524fcaedc9892347fc819
alibaghani <baghaniali2006@gmail.com>
2024-03-25 02:20:32 +0330

    implementing signup view

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import CreateView, RedirectView, DetailView
 
+from .models import User
 # Create your views here.
+
+
+class SignupView(CreateView):
+    model = User
+    fields = ['username','password','phone_number','biography','hobbies','gender']
+    success_url = reverse_lazy('signup')
+    template_name = 'signup.html'
+
+
