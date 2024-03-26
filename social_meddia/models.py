74e878c70480d55e60b5f701422bb5c19907d16e
alibaghani <baghaniali2006@gmail.com>
2024-03-26 13:43:16 +0330

    post  model was implemented

--- social_meddia/models.py
+++ social_meddia/models.py
@@ -5,3 +5,12 @@ class Follow(models.Model):
     follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
     following = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.CharField(max_length=250)
+    image = models.ImageField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
