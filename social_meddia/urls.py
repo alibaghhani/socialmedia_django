fa6552d2a5f2a1cf74bf5117a2564fac7486dd7e
alibaghani <baghaniali2006@gmail.com>
2024-03-28 02:25:48 +0330

    adding the post url

--- social_meddia/urls.py
+++ social_meddia/urls.py
@@ -1,4 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',HomepageView.as_view(),name='home'),
+    path('explore/', TemplateView.as_view(template_name='explore.html'), name='explore'),
+    path('settings/', TemplateView.as_view(template_name='settings.html'), name='settings'),
+    path('post/',UploadPostView.as_view(),name='post')
 
 
-urlpatter
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
