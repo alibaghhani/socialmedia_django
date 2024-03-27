2740a7eb91bff579aa5fe310ddb4fe5375f94918
alibaghani <baghaniali2006@gmail.com>
2024-03-28 02:23:32 +0330

    just clean the code

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', SigninView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
     path('signup/', SignupView.as_view(), name="signup"),
+    path('profile/<int:pk>/',ProfileView.as_view(),name='profile')
 ]
