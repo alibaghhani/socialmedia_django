313dbba73dad8a347e69b02f080278660b90b104
alibaghani <baghaniali2006@gmail.com>
2024-03-28 02:23:10 +0330

    some small changes

--- authentication/views.py
+++ authentication/views.py
@@ -6,13 +6,14 @@ from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import CreateView, RedirectView, DetailView
 from .models import User
+from social_meddia.models import *
 # Create your views here.
 
 
 class SignupView(CreateView):
     model = User
     fields = ['username','password','phone_number','biography','hobbies','gender']
-    success_url = reverse_lazy('signup')
+    success_url = reverse_lazy('home')
     template_name = 'signup.html'
 
     def form_valid(self, form):
@@ -22,7 +23,7 @@ class SignupView(CreateView):
 
 class SigninView(LoginView):
     model = User
-    next_page = reverse_lazy('signup')
+    next_page = reverse_lazy('home')
     template_name = 'login.html'
 
     def form_valid(self, form):
@@ -30,7 +31,19 @@ class SigninView(LoginView):
         return result
 
 class LogoutView(View,LoginRequiredMixin):
-    login_url = reverse_lazy('signin')
+    login_url = reverse_lazy('home')
     def get(self, request, *args, **kwargs):
         logout(request)
         return redirect(reverse_lazy('home'))
+
+class ProfileView(DetailView,LoginRequiredMixin):
+    login_url = reverse_lazy('login')
+    template_name = 'profile.html'
+    model = User
+    context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.get_object()
+        context['user_posts'] = Post.objects.filter(user=user)
+        return context
