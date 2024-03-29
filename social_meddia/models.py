7c98cc9865bc51fcda832108662be2430752f964
alibaghani <baghaniali2006@gmail.com>
2024-03-29 04:10:11 +0330

    implementing the imaage model

--- social_meddia/models.py
+++ social_meddia/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+
 from authentication.models import User
 
 class Follow(models.Model):
@@ -9,10 +11,13 @@ class Follow(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=250)
     content = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='media', default=None, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+def get_image_filename(instance,filename):
+    title = instance.post.title
+    slug = slugify(title)
+    return "post_images/%s-%s" % (slug, filename)
 
 
 class Like(models.Model):
@@ -26,3 +31,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='comments')
+
+class Image(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=get_image_filename, default=None, null=True, blank=True)
